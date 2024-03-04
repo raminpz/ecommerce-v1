a7f196e973027fe91bff0bc75fd9e0bd23de3b0e
Ramiro Nuñez <raminp.1012@gmail>
2024-03-04 16:51:50 -0500

    Initial commit

---
+++ src/main/java/com/softrami/ecommercev1/EcommerceV1Application.java
@@ -0,0 +1,13 @@
+package com.softrami.ecommercev1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceV1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/softrami/ecommercev1/EcommerceV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softrami.ecommercev1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
